7332d8e65bea021c75ab44297f4c2f12c6bfe309
Terrence Mahnken <terrencemm2@gmail.com>
2021-03-28 14:50:03 -0500

    set up mongodb connection using mongoose

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
-import { makeStyles } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
+import dbConnect from "../utils/db";
+import Business from "../models/Business";
 
 const style = {
     container: {
@@ -14,4 +15,14 @@ const style = {
 
 export default function Home() {
     return <Container style={style.container}>App</Container>;
+}
+
+/* Retrieves pet(s) data from mongodb database */
+export async function getServerSideProps() {
+    await dbConnect();
+
+    /* find all the data in our database */
+    const result = await Business.find({});
+
+    return { props: { businesses: result } };
 }
