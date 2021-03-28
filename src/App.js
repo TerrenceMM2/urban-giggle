17cca9cd1181b6f69082509c867e05a1526a1d84
Terrence Mahnken <terrencemm2@gmail.com>
2021-03-27 22:51:26 -0500

    remove React template; set up Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Box } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    root: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh",
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+
+    return <Container classes={{ root: classes.root }}>App</Container>;
 }
 
 export default App;
